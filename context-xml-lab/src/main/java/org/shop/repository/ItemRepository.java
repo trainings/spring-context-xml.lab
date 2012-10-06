3c70ae55a402f47fc26189c803d669671f867720
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2012-10-06 05:35:51 -0430

    Added JavaDocs to repositories

--- context-xml-lab/src/main/java/org/shop/repository/ItemRepository.java
+++ context-xml-lab/src/main/java/org/shop/repository/ItemRepository.java
@@ -4,11 +4,33 @@ import java.util.List;
 
 import org.shop.data.Item;
 
+/**
+ * Provides repository for manipulating items.
+ * 
+ * @author Dzmitry_Naskou
+ */
 public interface ItemRepository {
 	
+	/**
+	 * Creates the item.
+	 *
+	 * @param item the item
+	 * @return the item id
+	 */
 	Long createItem(Item item);
 	
+	/**
+	 * Delete item.
+	 *
+	 * @param itemId the item id
+	 */
 	void deleteItem(Long itemId);
 	
+	/**
+	 * Gets the items by order id.
+	 *
+	 * @param orderId the order id
+	 * @return the items by order id
+	 */
 	List<Item> getItemsByOrderId(Long orderId);
 }
