726ace36278dee027a8bc69fe6d854c475bf4aab
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2012-10-06 14:13:28 -0430

    Moved UserMap repository to factory class

---
+++ shop/src/main/java/org/shop/repository/factory/UserRepositoryFactory.java
@@ -0,0 +1,54 @@
+package org.shop.repository.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.shop.data.User;
+import org.shop.repository.UserRepository;
+import org.shop.repository.map.AbstractMapRepository;
+
+public final class UserRepositoryFactory {
+    
+    public UserRepository createUserRepository() {
+        return new UserMapRepository();
+    }
+    
+    /**
+     * 
+     * @author Dzmitry_Naskou
+     */
+    private class UserMapRepository extends AbstractMapRepository<User> implements UserRepository {
+
+        /* (non-Javadoc)
+         * @see org.shop.repository.UserRepository#getUserById(java.lang.Long)
+         */
+        @Override
+        public User getUserById(Long id) {
+            return get(id);
+        }
+
+        /* (non-Javadoc)
+         * @see org.shop.repository.UserRepository#createUser(org.shop.data.User)
+         */
+        @Override
+        public Long createUser(User user) {
+            return create(user);
+        }
+
+        /* (non-Javadoc)
+         * @see org.shop.repository.UserRepository#updateUser(org.shop.data.User)
+         */
+        @Override
+        public void updateUser(User user) {
+            update(user);
+        }
+
+        /* (non-Javadoc)
+         * @see org.shop.repository.UserRepository#getUsers()
+         */
+        @Override
+        public List<User> getUsers() {
+            return new ArrayList<User>(register.values());
+        }
+    }
+}
