653d4849c4f9deb49a2a47af176345ffede48676
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2012-10-07 02:59:54 -0430

    fixed proposal initializer id

--- shop/src/main/java/org/shop/ProposalInitializer.java
+++ shop/src/main/java/org/shop/ProposalInitializer.java
@@ -52,8 +52,8 @@ public class ProposalInitializer {
      * Inits the proposals.
      */
     public void initProposals() {
-        Seller amazon = sellerService.getSellerById((long) 0);
-        Seller samsung = sellerService.getSellerById((long) 1);
+        Seller amazon = sellerService.getSellerById((long) 1);
+        Seller samsung = sellerService.getSellerById((long) 2);
         
         Product galaxyTab = productService.getProductsByName(Products.SAMSUNG_GALAXY_TAB).get(0);
         Product kindleFire = productService.getProductsByName(Products.KINDLE_FIRE).get(0);
