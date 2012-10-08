7b4555acfeb560752919195815e8ebc7a2e7a49f
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2012-10-08 14:27:33 -0430

    Chnaged Order repositrory

--- shop/src/main/java/org/shop/repository/map/AbstractMapRepository.java
+++ shop/src/main/java/org/shop/repository/map/AbstractMapRepository.java
@@ -8,19 +8,63 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
 import org.shop.data.Entity;
 
+/**
+ * The Abstract Map Based Repository.
+ *
+ * @param <T> the generic type
+ * @author Dzmitry_Naskou
+ * 
+ * @see Map
+ */
 public class AbstractMapRepository<T extends Entity> {
+    
+    /** The sequence. */
     private long sequence = 0;
     
     protected final Map<Long, T> register = new HashMap<Long, T>();
     
+    /**
+     * Instantiates a new map based repository.
+     */
+    public AbstractMapRepository() {
+        this(0);
+    }
+
+    /**
+     * Instantiates a new map based repository.
+     *
+     * @param initialSequence the initial sequence value
+     */
+    public AbstractMapRepository(long initialSequence) {
+        super();
+        this.sequence = initialSequence;
+    }
+
+    /**
+     * Get next primary key.
+     *
+     * @return the long
+     */
     protected final Long nextPk() {
         return ++sequence;
     }
     
+    /**
+     * Gets the object by id.
+     *
+     * @param id the id
+     * @return the object
+     */
     protected T get(Long id) {
         return register.get(id);
     }
     
+    /**
+     * Save object to datasource.
+     *
+     * @param entity the entity
+     * @return the object id
+     */
     protected Long create(T entity) {
         Long id = nextPk();
         
@@ -31,16 +75,34 @@ public class AbstractMapRepository<T extends Entity> {
         return id;
     }
     
+    /**
+     * Update the entity.
+     *
+     * @param entity the entity
+     */
     protected void update(T entity) {
         if (entity.getId() != null) {
             register.put(entity.getId(), entity);
         }
     }
     
+    /**
+     * Delete the object by id.
+     *
+     * @param id the id
+     */
     protected void delete(Long id) {
         register.remove(id);
     }
     
+    /**
+     * Select objects by predicate.
+     *
+     * @param predicate the predicate
+     * @return the list
+     * 
+     * @see Predicate
+     */
     @SuppressWarnings("unchecked")
     protected List<T> select(Predicate predicate) {
         return (List<T>)CollectionUtils.select(register.values(), predicate);
--- shop/src/main/java/org/shop/repository/map/OrderMapRepository.java
+++ shop/src/main/java/org/shop/repository/map/OrderMapRepository.java
@@ -6,7 +6,21 @@ import org.apache.commons.collections.Predicate;
 import org.shop.data.Order;
 import org.shop.repository.OrderRepository;
 
+/**
+ * The Class OrderMapRepository.
+ * 
+ * @author Dzmitry_Naskou
+ */
 public class OrderMapRepository extends AbstractMapRepository<Order> implements OrderRepository {
+
+    /**
+     * Instantiates a new order map based repository.
+     *
+     * @param initialSequence the initial sequence
+     */
+    public OrderMapRepository(long initialSequence) {
+        super(initialSequence);
+    }
 
     /* (non-Javadoc)
      * @see org.shop.repository.OrderRepository#getOrderById(java.lang.Long)
@@ -40,15 +54,27 @@ public class OrderMapRepository extends AbstractMapRepository<Order> implements 
         return select(new OrderByUserPredicate(userId));
     }
     
+    /**
+     * The Class OrderByUserPredicate.
+     */
     private class OrderByUserPredicate implements Predicate {
         
+        /** The user id. */
         private Long userId;
 
+        /**
+         * Instantiates a new order by user predicate.
+         *
+         * @param userId the user id
+         */
         private OrderByUserPredicate(Long userId) {
             super();
             this.userId = userId;
         }
 
+        /* (non-Javadoc)
+         * @see org.apache.commons.collections.Predicate#evaluate(java.lang.Object)
+         */
         @Override
         public boolean evaluate(Object input) {
             if (input instanceof Order) {
