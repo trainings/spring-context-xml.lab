2c0bd241beedf7433e0aae9a3bf20322ae02f29c
Dzmitry_Naskou <noskov.d@gmail.com>
2012-11-25 07:59:05 -0430

    Added javadocs

--- shop/src/main/java/org/shop/repository/factory/UserRepositoryFactory.java
+++ shop/src/main/java/org/shop/repository/factory/UserRepositoryFactory.java
@@ -7,8 +7,21 @@ import org.shop.data.User;
 import org.shop.repository.UserRepository;
 import org.shop.repository.map.AbstractMapRepository;
 
+/**
+ * A factory for creating UserRepository objects.
+ * 
+ * @author Dzmitry_Naskou
+ * 
+ * @see UserRepository
+ * @see User
+ */
 public final class UserRepositoryFactory {
     
+    /**
+     * Creates a new UserRepository instance.
+     *
+     * @return the user repository
+     */
     public UserRepository createUserRepository() {
         return new UserMapRepository();
     }
